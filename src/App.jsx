0ad1760b0decb6658f7741d8050950a27279a0a8
eastberry <albanus@eastberry.io>
2024-02-01 17:32:06 +0300

    added navbar & edited sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import Sidebar from './components/sidebar'
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Sidebar/>
+      <Navbar/>
     </BrowserRouter>
   )
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { MdOutlineDashboard } from "react-icons/md";
 import { MdOutlineInventory } from "react-icons/md";
-import { MdOutlineProductionQuantityLimits } from "react-icons/md";
 import { IoBagAdd } from "react-icons/io5";
 import { FaBarcode } from "react-icons/fa";
 import { FcSalesPerformance } from "react-icons/fc";
@@ -9,17 +8,29 @@ import { BiSolidPurchaseTag } from "react-icons/bi";
 import { ChevronsRight } from "lucide-react";
 import { GrDeliver } from "react-icons/gr";
 import { CiReceipt } from "react-icons/ci";
+import { ChevronDown } from "lucide-react";
 import { Link } from "react-router-dom";
-import { IoIosAppstore } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import { BsCashCoin } from "react-icons/bs";
 
 const Sidebar = ({ togglePartnersTable }) => {
+  const [open, setOpen] = useState(false);
+
+  const handlePartnersClick = () => {
+    togglePartnersTable();
+  };
+
+  const [inventoryOpen, setInventoryOpen] = useState(false);
+
   const menus = [
     { name: "Dashboard", link: "/", icon: MdOutlineDashboard },
-    { name: "Inventory", link: "/", icon: MdOutlineInventory },
-    { name: "Products", link: "/", icon: MdOutlineProductionQuantityLimits },
-    { name: "Create Product", link: "/", icon: IoBagAdd },
+    {
+      name: "Inventory",
+      link: "/",
+      icon: MdOutlineInventory,
+      items: ["Materials", "Accessories"],
+    },
+    { name: "Products", link: "/", icon: IoBagAdd },
     { name: "Print Barcode", link: "/", icon: FaBarcode },
     { name: "Sales", link: "/", icon: FcSalesPerformance },
     { name: "Purchases", link: "/", icon: BiSolidPurchaseTag },
@@ -27,65 +38,95 @@ const Sidebar = ({ togglePartnersTable }) => {
     { name: "Expenses", link: "/", icon: BsCashCoin },
     { name: "Suppliers", link: "/", icon: GrDeliver },
     { name: "Profile", link: "/", icon: CgProfile, margin: true },
+    { name: "Profile", link: "/", icon: CgProfile, margin: true },
+    { name: "Profile", link: "/", icon: CgProfile, margin: true },
+    { name: "Profile", link: "/", icon: CgProfile, margin: true },
+    { name: "Profile", link: "/", icon: CgProfile, margin: true },
+    { name: "Profile", link: "/", icon: CgProfile, margin: true },
   ];
 
-  const handlePartnersClick = () => {
-    togglePartnersTable();
-  };
-
-  const [open, setOpen] = useState(false);
-
   return (
-    <div className="flex flex-col md:flex-row gap-6 h-[100vh] relative">
+    <div className="flex fixed flex-col md:flex-row gap-6 h-[100vh] mt-16">
       <div
-        className={`bg-[#4664fd] min-h-screen ${
+        className={`bg-[#4664fd] overflow-y-scroll min-h-screen ${
           open ? "w-60" : "w-16"
-        } duration-500 text-gray-100 px-4 sticky top-0`}
+        } duration-500 text-gray-100 px-4 sticky top-0 custom-scrollbar`}
         onMouseEnter={() => setOpen(true)}
         onMouseLeave={() => setOpen(false)}
       >
-        {open && (
+        {/* {open && (
           <ChevronsRight
             size={26}
-            className="cursor-pointer rounded-full bg-black absolute top-4 right-0 text-white -mr-2 z-10"
+            className="cursor-pointer rounded-full border border-white bg-orange-400 absolute top-4 right-0 text-white -mr-2 z-10"
             onClick={() => setOpen(!open)}
           />
-        )}
+        )} */}
         <div className="mt-4 top-8 flex flex-col gap-4 relative">
           {menus?.map((menu, i) => (
-            <Link
-              to={menu?.link}
-              key={i}
-              onClick={menu.name === "Partners" ? handlePartnersClick : null}
-              className={`group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md ${
-                menu?.margin ? "mt-20" : ""
-              }`}
-              open={open}
-            >
-              <div>{React.createElement(menu?.icon, { size: "20" })}</div>
-              <h2
-                className={`whitespace-pre duration-500 ${
-                  !open ? "opacity-0 translate-x-28 overflow-hidden" : ""
-                }`}
+            <div key={i}>
+              {menu.name === "Inventory" ? (
+                <div
+                className={`group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md cursor-pointer relative`}
+                onClick={() => setInventoryOpen(!inventoryOpen)}
               >
-                {menu?.name}
-              </h2>
-              <h2
-                className={`${
-                  open
-                    ? "hidden"
-                    : `absolute left-14 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`
-                }`}
-              >
-                {menu?.name}
-              </h2>
-            </Link>
+                <div>{React.createElement(menu.icon, { size: "20" })}</div>
+                <h2
+                  className={`whitespace-pre duration-500 ${
+                    !open ? "opacity-0 translate-x-16" : ""
+                  }`}
+                >
+                  {menu.name}
+                </h2>
+                <ChevronDown size={16} className="ml-10 text-gray-300" />
+              </div>
+              
+              ) : (
+                <Link
+                  to={menu?.link}
+                  key={i}
+                  onClick={
+                    menu.name === "Partners" ? handlePartnersClick : null
+                  }
+                  className={`group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md ${
+                    menu?.margin ? "mt-4" : ""
+                  }`}
+                  open={open}
+                >
+                  <div>{React.createElement(menu?.icon, { size: "20" })}</div>
+                  <h2
+                    className={`whitespace-pre duration-500 ${
+                      !open ? "opacity-0 translate-x-28 overflow-hidden" : ""
+                    }`}
+                  >
+                    {menu?.name}
+                  </h2>
+                  <h2
+                    className={`${
+                      open
+                        ? "hidden"
+                        : `absolute left-14 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`
+                    }`}
+                  >
+                    {menu?.name}
+                  </h2>
+                </Link>
+              )}
+              {menu.name === "Inventory" &&
+                inventoryOpen &&
+                menu.items.map((item, index) => (
+                  <Link
+                    to={`/inventory/${item.toLowerCase()}`}
+                    key={index}
+                    className="group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md ml-6 transition duration-500 ease-in-out"
+                    >
+                    <div></div>
+                    <h2>{item}</h2>
+                  </Link>
+                ))}
+            </div>
           ))}
         </div>
       </div>
-      {/* <div className="md:mt-5 md:text-center md:justify-center text-xl text-gray-900 font-semibold">
-        Dashboard
-      </div> */}
     </div>
   );
 };
